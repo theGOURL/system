25e21bd6595583d73ba43757932a509c99ca31a0
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-01-18 20:05:19 -0300

    Go mod Init System

---
+++ commands/exec.go
@@ -0,0 +1,16 @@
+package commands
+
+import (
+	"fmt"
+	"os/exec"
+
+	"github.com/theGOURL/go_url/warning"
+)
+
+// this function executes operating system commands
+func Execute(command string) {
+	out, err := exec.Command(command).Output()
+	warning.PRINT_DEFAULT_ERRORS(err, "INVALID COMMAND")
+	output := string(out[:])
+	fmt.Println(output)
+}
